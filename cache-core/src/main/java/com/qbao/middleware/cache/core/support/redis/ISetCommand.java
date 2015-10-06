dac43d4d9a0f0f9c912f2b61df8e1736b479b963
Yate <e.yangting@gmail.com>
2015-10-06 14:26:28 +0800

    redis set type
    Signed-off-by: Yate <e.yangting@gmail.com>

--- cache-core/src/main/java/com/qbao/middleware/cache/core/support/redis/ISetCommand.java
+++ cache-core/src/main/java/com/qbao/middleware/cache/core/support/redis/ISetCommand.java
@@ -4,6 +4,7 @@
 package com.qbao.middleware.cache.core.support.redis;
 
 import com.qbao.middleware.cache.core.ICacheCommand;
+import com.qbao.middleware.cache.exception.CacheCodeException;
 
 /**
  * @author Yate
@@ -33,4 +34,25 @@ public interface ISetCommand extends ICacheCommand {
 //    ZRANGEBYLEX
 //    ZLEXCOUNT
 //    ZREMRANGEBYLEX
+    
+    public void zAdd()throws CacheCodeException;
+    public void zCard()throws CacheCodeException;
+    public void zCount()throws CacheCodeException;
+    public void zIncrBy()throws CacheCodeException;
+    public void zRange()throws CacheCodeException;
+    public void zRangeByScore()throws CacheCodeException;
+    public void zRank()throws CacheCodeException;
+    public void zRem()throws CacheCodeException;
+    public void zRemRangeByRank()throws CacheCodeException;
+    public void zRemRangeByScore()throws CacheCodeException;
+    public void zRevRange()throws CacheCodeException;
+    public void zRevRangeByScore()throws CacheCodeException;
+    public void zRevRank()throws CacheCodeException;
+    public void zScore()throws CacheCodeException;
+    public void zUnionStore()throws CacheCodeException;
+    public void zInterStore()throws CacheCodeException;
+    public void zScan()throws CacheCodeException;
+    public void zRangeByLex()throws CacheCodeException;
+    public void zLexCount()throws CacheCodeException;
+    public void zRemRangeByLex()throws CacheCodeException;
 }
